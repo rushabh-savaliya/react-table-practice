317bc0477c4ec25d7367a929e3b03ba606f9ab45
rushabh-savaliya <rushabh1245@gmail.com>
2024-10-19 19:18:54 +0530

    add filter by colum

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 function App() {
   const [user_data, setPosts] = useState([]);
+  const [getSeachText, setSeachText] = useState("");
+  const [getDropDownValue, setDropDownValue] = useState("title");
 
   useEffect(() => {
     get_date();
@@ -12,14 +14,40 @@ function App() {
       let user_data = await axios.get(
         "https://jsonplaceholder.typicode.com/todos/"
       );
-      console.log(user_data);
       setPosts(user_data.data);
     } catch (error) {
       console.log(error);
     }
   };
+  const filterData = (ele) => {
+    const searchText = getSeachText.toLowerCase();
+    switch (getDropDownValue) {
+      case "id":
+      case "userId":
+        return ele[getDropDownValue].toString().includes(searchText);
+      case "title":
+        return ele.title.toLowerCase().includes(searchText);
+      case "completed":
+        return ele.completed.toString().toLowerCase().includes(searchText);
+      default:
+        return false;
+    }
+  };
   return (
     <>
+      <select onChange={(e) => setDropDownValue(e.target.value)}>
+        <option value="id">id</option>
+        <option value="userId">userId</option>
+        <option value="title">title</option>
+        <option value="completed">completed</option>
+      </select>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => {
+          setSeachText(e.target.value);
+        }}
+      />
       <table>
         <tr>
           <th>id</th>
@@ -27,7 +55,7 @@ function App() {
           <th>title</th>
           <th>completed</th>
         </tr>
-        {user_data.map((ele) => (
+        {user_data.filter(filterData).map((ele) => (
           <tr key={ele.id}>
             <td>{ele.id}</td>
             <td>{ele.userId}</td>
